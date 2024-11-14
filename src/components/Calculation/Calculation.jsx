04a13c0646ce3a28e2d5c64d8f98f682b2baa1db
Mary Anzhur <maryanzhur@Marys-Laptop.local>
2024-11-15 00:19:37 +0400

    added return to the component

--- src/components/Calculation/Calculation.jsx
+++ src/components/Calculation/Calculation.jsx
@@ -1,15 +1,17 @@
 import "./Calculation.css";
 
 function Calculation() {
-  <div>
-    <div className="numberArea"></div>
-    <div className="action">
-      <button className="calcul">+1</button>
-      <button className="calcul">-1</button>
-      <button className="calcul">x2</button>
-      <button className="calcul">/2</button>
+  return (
+    <div>
+      <div className="numberArea"></div>
+      <div className="action">
+        <button className="calcul">+1</button>
+        <button className="calcul">-1</button>
+        <button className="calcul">x2</button>
+        <button className="calcul">/2</button>
+      </div>
     </div>
-  </div>
+  );
 }
 
 export default Calculation;
