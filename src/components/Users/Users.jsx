51266a8562f7d94b735fa70dea244c4bafdd8fdb
Mary Anzhur <maryanzhur@Marys-Laptop.local>
2024-11-20 02:34:08 +0400

    only password hide/show have problem

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,10 +3,14 @@ import see from "../../Assets/img/eye_op.svg";
 import hide from "../../Assets/img/eye-cl.svg";
 import a from "../../Assets/img/a_z.svg";
 import z from "../../Assets/img/z_a.svg";
+import num1 from "../../Assets/img/1_9.svg";
+import num9 from "../../Assets/img/9_1.svg";
 
 import "./Users.css";
 
 function Users() {
+  const [user, setUser] = useState([]);
+
   useEffect(() => {
     function getData() {
       fetch("https://673b986796b8dcd5f3f6e23a.mockapi.io/dataForReact")
@@ -16,42 +20,103 @@ function Users() {
     getData();
   }, []);
 
-  const [user, setUser] = useState([]);
-  const [password, setPassword] = useState(true);
+  // console.log(user);
+
   const [id, setId] = useState(true);
   const [name, setName] = useState(true);
   const [age, setAge] = useState(true);
   const [date, setDate] = useState(true);
-  const [button, setButton] = useState(user);
+  const [password, setPassword] = useState(0);
 
-  // let idSort = 0;
-
-  const seeHidePass = () => {
-    setPassword(!password);
+  const seeHidePass = (userId) => {
+    let updatedArr = [];
+    if (password === 0) {
+      updatedArr = user.map((item) => ({
+        ...item,
+        isOpen: item.id === userId,
+      }));
+      setPassword(1);
+    } else {
+      updatedArr = user.map((item) => ({
+        ...item,
+        isOpen: false,
+      }));
+      setPassword(0);
+    }
+    setUser(updatedArr);
   };
 
   const sortId = () => {
-    setId(!id);
-    // if (idSort === 0 || idSort === -1) {
-
-    // }
+    let sortedById = [];
+    if (id === true) {
+      sortedById = user.toSorted((a, b) => {
+        return a.id - b.id;
+      });
+      setId(!id);
+    } else if (id === false) {
+      sortedById = user.toSorted((a, b) => {
+        return b.id - a.id;
+      });
+      setId(!id);
+    }
+    setUser(sortedById);
   };
 
   const sortName = () => {
-    setName(!name);
+    let sortedByName = [];
+    if (name === true) {
+      sortedByName = user.toSorted((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+      setName(!name);
+    } else if (name === false) {
+      sortedByName = user.toSorted((a, b) => {
+        return b.name.localeCompare(a.name);
+      });
+      setName(!name);
+    }
+    setUser(sortedByName);
   };
 
   const sortAge = () => {
-    setAge(!age);
+    let sortedByAge = [];
+    if (age === true) {
+      sortedByAge = user.toSorted((a, b) => {
+        return a.age - b.age;
+      });
+      setAge(!age);
+    } else if (age === false) {
+      sortedByAge = user.toSorted((a, b) => {
+        return b.age - a.age;
+      });
+      setAge(!age);
+    }
+    setUser(sortedByAge);
   };
 
   const sortDate = () => {
-    setDate(!date);
+    let sortedByDate = [];
+    if (date === true) {
+      sortedByDate = user.toSorted((a, b) => {
+        return (
+          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+        );
+      });
+      setDate(!date);
+    } else if (date === false) {
+      sortedByDate = user.toSorted((a, b) => {
+        return (
+          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        );
+      });
+      setDate(!date);
+    }
+    setUser(sortedByDate);
   };
 
   const deleteButton = (userId) => {
-    let sorted = button.filter((elm) => elm.id !== userId);
-    return setButton(sorted);
+    let sorted = user.filter((elm) => elm.id !== userId);
+    return setUser(sorted);
   };
 
   return (
@@ -59,7 +124,7 @@ function Users() {
       <div className="heading">
         <div className="withImg">
           <p>ID</p>
-          <img src={id ? a : z} alt="a to z icon" onClick={sortId} />
+          <img src={id ? num1 : num9} alt="a to z icon" onClick={sortId} />
         </div>
         <p>Photo</p>
         <div className="withImg">
@@ -68,31 +133,31 @@ function Users() {
         </div>
         <div className="withImg">
           <p>Age</p>
-          <img src={age ? a : z} alt="a to z icon" onClick={sortAge} />
+          <img src={age ? num1 : num9} alt="a to z icon" onClick={sortAge} />
         </div>
-        <div className="withImg">
-          <p>Password</p>
-          <img
-            src={password ? see : hide}
-            alt="eye icon"
-            onClick={seeHidePass}
-          />
-        </div>
+        <p>Password</p>
         <div className="withImg">
           <p>Created Date</p>
-          <img src={date ? a : z} alt="a to z icon" onClick={sortDate} />
+          <img src={date ? num1 : num9} alt="a to z icon" onClick={sortDate} />
         </div>
         <p>Delete Button</p>
       </div>
       <div className="userData">
-        {button.map((elm, index) => {
+        {user.map((elm, index) => {
           return (
             <div className="userBox" key={index}>
               <p>{elm.id}</p>
               <img src={elm.avatar} alt="city" />
               <p>{elm.name}</p>
               <p>{elm.age}</p>
-              <p className="passP">{elm.password}</p>
+              <div className="withImg">
+                <p>{!elm.isOpen ? "**************" : elm.password}</p>
+                <img
+                  src={!elm.isOpen ? see : hide}
+                  alt="eye icon"
+                  onClick={() => seeHidePass(elm.id)}
+                />
+              </div>
               <p>{elm.createdAt}</p>
               <button
                 className="deleteBtn"
