82e82322c73482dfa54c337915f6f1b8ca2a10a8
Mary Anzhur <maryanzhur@Marys-Laptop.local>
2024-11-27 22:20:53 +0400

    lesson 11 done

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -7,7 +7,19 @@ function Body({ data, path }) {
         <img src={elm.image} alt="" />
         <p>{elm.title}</p>
         <p>Price: ${elm.price}</p>
-      </div> : null))}
+      </div> : path === "users" ? <div key={index} className="boxes">
+        <img src={elm.image} alt="" />
+        <p>{elm.firstName}</p>
+        <p>Age: {elm.age}</p>
+      </div> : path === "comments" ? <div key={index} className="boxes">
+        <p>{elm.body}</p>
+        <p>{elm.postId}</p>
+        <p>Like: {elm.likes}</p>
+      </div> :path === "todos" ? <div key={index} className="boxes">
+        <p>{elm.id}</p>
+        <p>{elm.userId}</p>
+        <p>{elm.title}</p>
+      </div>: null))}
     </div>
   );
 }
