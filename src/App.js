062f99f87a94ada6bb3fb71b0e9be331bb4129aa
Mary Anzhur <maryanzhur@Marys-Laptop.local>
2024-12-10 23:30:01 +0400

    problem with fetch of db.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Data from "./conponents/Data/Data";
+import Form from "./conponents/Form/Form";
+
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return <div>
+    <Form />
+    <Data />
+  </div>;
 }
 
 export default App;
---
+++ src/conponents/Form/Form.jsx
@@ -0,0 +1,9 @@
+import './Form.css'
+
+const Form = () =>{
+    return(
+        <div></div>
+    )
+}
+
+export default Form
